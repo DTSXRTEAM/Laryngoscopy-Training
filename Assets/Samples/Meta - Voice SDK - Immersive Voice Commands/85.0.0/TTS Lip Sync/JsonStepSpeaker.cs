9914c49e02a74b6529ab58a41bab0d3b2e7ee1da
DTSXRTEAM <137754320+DTSXRTEAM@users.noreply.github.com>
2026-03-06 13:31:15 +0530

    TTS Updated

--- Assets/Samples/Meta - Voice SDK - Immersive Voice Commands/85.0.0/TTS Lip Sync/JsonStepSpeaker.cs
+++ Assets/Samples/Meta - Voice SDK - Immersive Voice Commands/85.0.0/TTS Lip Sync/JsonStepSpeaker.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Meta.WitAi.TTS.Utilities;
@@ -7,32 +8,40 @@ public class JsonStepSpeaker : MonoBehaviour
 {
     public TTSSpeaker speaker;
     public TextAsset jsonFile;
+
+    public float firstSentenceDelay = 3f;   // Delay before first sentence
+
     private List<string> steps = new List<string>();
     private int currentStep = 0;
 
     void Start()
-
     {
         LoadSteps();
+        StartCoroutine(SpeakFirstSentence());
     }
 
+    IEnumerator SpeakFirstSentence()
+    {
+        yield return new WaitForSeconds(firstSentenceDelay);
+        SpeakNext();
+    }
 
     void LoadSteps()
-
     {
         if (jsonFile == null)
-
         {
             Debug.LogError("JSON file missing");
             return;
         }
-        StepContainer container = JsonUtility.FromJson<StepContainer>(jsonFile.text);
-      for (int i = 0; i < container.steps.Length; i++)
 
+        StepContainer container = JsonUtility.FromJson<StepContainer>(jsonFile.text);
+
+        for (int i = 0; i < container.steps.Length; i++)
         {
             steps.Add(container.steps[i].text);
         }
     }
+
     public void SpeakNext()
     {
         if (currentStep >= steps.Count)
@@ -40,21 +49,19 @@ public class JsonStepSpeaker : MonoBehaviour
             Debug.Log("All steps completed");
             return;
         }
+
         speaker.Speak(steps[currentStep]);
         currentStep++;
     }
+
     public void ResetSteps()
     {
-
         currentStep = 0;
-
     }
-
 }
 
 [Serializable]
 public class StepContainer
-
 {
     public Step[] steps;
 }
